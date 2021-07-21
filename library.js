00d02e38af2555e241ad6073289541b3fe6aede5
tatami <tatami_gaeshi@mm.st>
2021-07-20 18:05:47 -0700

    Add basic removal functionality

--- library.js
+++ library.js
@@ -9,38 +9,55 @@ function Book(title, author, pages, read){
 	this.pages = pages;
 	this.read = read;
 	this.info = function(){
-		console.log(title);
-	};
-}
+	console.log(title);
+	}; 
+}   
+   
+   
+  
+function removeBook(book){
+//TODO: also remove entry in array
+} 
 
 function addBookToLibrary(book){
 	myLibrary.push(book);
 
-	const libBook = document.createElement(`div`);
+	let libBook = document.createElement(`div`);
+	libBook.classList.add(`book`);
 
 	let bookTitle = document.createElement("p");
 	let bookAuthor = document.createElement("p");
 	let bookPages = document.createElement("p");
 	let bookRead = document.createElement("p");
-	
+	let bookRM = document.createElement("button");
+
+	bookRM.addEventListener(`click`, function(e){
+		console.log(et);
+		e.target.parentElement.remove();
+	});
 
 	libBook.textContent = "TESTING";
-	bookTitle.textContent = testBook.title;
-	bookAuthor.textContent = testBook.author;
-	bookPages.textContent = testBook.pages;
-	bookRead.textContent = testBook.read;
+	bookTitle.textContent = book.title;
+	bookAuthor.textContent = book.author;
+	bookPages.textContent = book.pages;
+	bookRead.textContent = book.read;
+	bookRM.textContent = "REMOVE";
 
 	libBook.appendChild(bookTitle);
 	libBook.appendChild(bookAuthor);
 	libBook.appendChild(bookPages);
 	libBook.appendChild(bookRead);
+	libBook.appendChild(bookRM);
+	
 
 	container.appendChild(libBook);
 }
 
 let testBook = new Book("TITLE", "AUTHOR", "150", "READ");
+let testBook2 = new Book("TITLE2", "AUTHOR2", "1502", "READ2");
 addBookToLibrary(testBook);
 
+addBookToLibrary(testBook2);
 
 
 
