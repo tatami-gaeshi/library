c998306b2afa6ec6947c908693a63186fdfffab3
tatami <tatami_gaeshi@mm.st>
2021-07-22 15:09:02 -0700

    Allow adding new books to page

--- library.js
+++ library.js
@@ -2,6 +2,7 @@
 
 const container = document.querySelector(`#container`);
 let myLibrary = [];
+let idx = 0;
 
 function Book(title, author, pages, read){
 	this.title = title;
@@ -32,7 +33,7 @@ function addBookToLibrary(book){
 	let bookRM = document.createElement("button");
 
 	bookRM.addEventListener(`click`, function(e){
-		console.log(et);
+		console.log(e);
 		e.target.parentElement.remove();
 	});
 
@@ -48,7 +49,6 @@ function addBookToLibrary(book){
 	libBook.appendChild(bookPages);
 	libBook.appendChild(bookRead);
 	libBook.appendChild(bookRM);
-	
 
 	container.appendChild(libBook);
 }
@@ -59,6 +59,23 @@ addBookToLibrary(testBook);
 
 addBookToLibrary(testBook2);
 
+const submit = document.querySelector(`#submit`);
+submit.addEventListener('click', function(e){
+
+	console.log(document.querySelector('#pages'));
+	e.preventDefault();
+	console.log(e);
+
+	let tit = document.querySelector('#title').value;
+	let aut = document.querySelector('#author').value;
+	let num = document.querySelector('#pages').value;
+	let rea = document.querySelector('#read').value;
+
+	let newBook = new Book(tit, aut, num, rea);
+	addBookToLibrary(newBook);
+
+	document.getElementById('book-input').reset();
+});
 
 
 //let testVal = 6;
